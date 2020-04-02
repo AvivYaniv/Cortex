500a88c7dfca22999461adbe9d95533ea0002edd
AvivYaniv <avivyaniv@gmx.com>
2020-04-02 13:50:28 +0300

    Minor changes
    * Removed unused __main__ of cortex package (as it was seperated)
    * Update description in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name = 'Cortex',
     version = '0.1.0',
     author = 'Aviv Yaniv',
-    description = 'Cortex',
+    description = 'Brain machine interface with extreme flexibility and scalability',
     packages = find_packages(),
     install_requires = ['click', 'flask'],
     tests_require = ['pytest', 'pytest-cov'],
