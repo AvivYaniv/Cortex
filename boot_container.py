30d105fa6e075742d36048e4f78b55fc42cedfbe
AvivYaniv <avivyaniv@gmx.com>
2020-04-03 20:02:35 +0300

    Flexability improvment of Docker integration
    Now services can run just by specifing the environment variable, and their specific-boot would happen automatically

--- boot_container.py
+++ boot_container.py
@@ -1,39 +1,56 @@
 import os
+import sys
 
-from cortex.server import run_server
-
-from cortex.client import upload_sample
-
-from cortex.readers.reader_versions import ReaderVersions
-
-print("Hello Sabich! ")
+# Constants Section
+CONTAINER_NAME_ENVIRONMENT_VARIABLE                 =   'RUN'      
+PROJECT_BASE_DIRECTORY                              =   'cortex'
+CONTAINER_BOOT_FILE_NAME                            =   'boot_container.py'
 
 # Messages Section
-UNKNOWN_COMMAND_TO_RUN_ERROR_MESSAGE    =   'Unknown command to run, environment variable is illegal'
-RUNNING_COMMAND_INFO_MESSAGE            =   'Running command...'
+NO_CONTAINER_SPECIFIED_ERROR_MESSAGE                =   'ERROR: No container specified in environment variable ' + CONTAINER_NAME_ENVIRONMENT_VARIABLE 
+CONTAINER_DIRECTORY_NOT_FOUND_ERROR_MESSAGE_FRMT    =   'ERROR: Container directory {} not found'
+CONTAINER_BOOT_FILE_NOT_FOUND_ERROR_MESSAGE_FRMT    =   'ERROR: Container boot file {} not found'
+BOOTING_CONTAINER_INFO_MESSAGE                      =   'Booting container...'
 
-# Functions Section
-def run_client_command():
-    file_path    	= 	'sample.mind.gz'
-    host, port 		= 	'127.0.0.1', '8000'
-    upload_sample(host, port, file_path=file_path, version=ReaderVersions.PROTOBUFF)
+class ERROR_CODES:
+    NO_ERROR                        =   0
+    NO_NO_CONTAINER_SPECIFIED       =   -1
+    CONTAINER_DIRECTORY_NOT_FOUND   =   -2
+    CONTAINER_BOOT_FILE_NOT_FOUND   =   -3
 
-def run_server_command():
-    host, port 		= 	'127.0.0.1', '8000'
-    run_server(host, port)
 
-# Variables Section
-RUN_FUNCTIONS_DICTIONARY =              \
-    {                                   \
-        'SERVER' : run_server_command,  \
-        'CLIENT' : run_client_command   \
-    }
+def get_container_path(container_name):
+    return os.path.join(PROJECT_BASE_DIRECTORY, container_name.lower())
+
+def get_container_boot_file_path(container_path, container_boot_file_name=CONTAINER_BOOT_FILE_NAME):
+    return os.path.join(container_path, container_boot_file_name)
 
 if "__main__" == __name__:
-    run_command = os.environ['RUN']
+    # Validating container has been specified
+    if CONTAINER_NAME_ENVIRONMENT_VARIABLE not in os.environ:
+        print(NO_CONTAINER_SPECIFIED_ERROR_MESSAGE)
+        sys.exit(ERROR_CODES.NO_NO_CONTAINER_SPECIFIED)
+        
+    # Fetching environment variable to determine which code to run
+    container_name = os.environ[CONTAINER_NAME_ENVIRONMENT_VARIABLE]
     
-    if (run_command not in RUN_FUNCTIONS_DICTIONARY):
-        print(UNKNOWN_COMMAND_TO_RUN_ERROR_MESSAGE)
-    else:
-        print(RUNNING_COMMAND_INFO_MESSAGE)
-        RUN_FUNCTIONS_DICTIONARY[run_command]()
+    # Building container path
+    container_path = get_container_path(container_name)
+    
+    # If container folder dosen't exist
+    if (not os.path.isdir(container_path)):
+        print(CONTAINER_DIRECTORY_NOT_FOUND_ERROR_MESSAGE_FRMT.format(container_path))
+        sys.exit(ERROR_CODES.CONTAINER_DIRECTORY_NOT_FOUND)
+    
+    # Building container boot file path
+    container_boot_file_path = get_container_boot_file_path(container_path)
+    
+    # If container folder dosen't exist
+    if (not os.path.isfile(container_boot_file_path)):
+        print(CONTAINER_BOOT_FILE_NOT_FOUND_ERROR_MESSAGE_FRMT.format(container_boot_file_path))
+        sys.exit(ERROR_CODES.CONTAINER_BOOT_FILE_NOT_FOUND)
+    
+    # Booting container
+    print(BOOTING_CONTAINER_INFO_MESSAGE)
+    exec(open(container_boot_file_path).read())
+    
