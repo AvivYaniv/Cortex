318bce0e59fd892e85f45739ef27f21a75c14a65
AvivYaniv <avivyaniv@gmx.com>
2020-04-10 16:59:50 +0300

    Implemented DataBase for Cortex project

--- cortex/database/database_cortex.py
+++ cortex/database/database_cortex.py
@@ -13,16 +13,19 @@ logger_loader.load_log_config()
 class _DataBaseCortex(_DataBaseBase):
     # Constants Section
     # Entities
-    ENTITY_USER            =   'user'
-    ENTITY_SNAPSHOT        =   'snapshot'
-    ENTITY_POSE            =   'pose'
-    ENTITY_USER_FEELINGS   =   'user_feelings'
-    ENTITY_COLOR_IMAGE     =   'color_image'
-    ENTITY_DEPTH_IMAGE     =   'depth_image'
+    ENTITY_USER                 =   'user'
+    ENTITY_SNAPSHOT             =   'snapshot'
+    
+    ENTITY_POSE                 =   'pose'
+    ENTITY_USER_FEELINGS        =   'user_feelings'
+    ENTITY_COLOR_IMAGE          =   'color_image'
+    ENTITY_DEPTH_IMAGE          =   'depth_image'
 
-    ENTITY_IDS_NAMES       =    { ENTITY_USER : 'user_id', ENTITY_SNAPSHOT : 'snapshot_uuid' }
+    AVAILABLE_RESULTS_LIST_NAME =    'aviable_results'
 
-    PARSED_ENTITIES_LIST   =    [ ENTITY_POSE, ENTITY_USER_FEELINGS, ENTITY_COLOR_IMAGE, ENTITY_DEPTH_IMAGE ]
+    ENTITY_IDS_NAMES            =    { ENTITY_USER : 'user_id', ENTITY_SNAPSHOT : 'snapshot_uuid' }
+
+    PARSED_ENTITIES_LIST        =    [ ENTITY_POSE, ENTITY_USER_FEELINGS, ENTITY_COLOR_IMAGE, ENTITY_DEPTH_IMAGE ]
 
     # Methods Section
     # User Entity Section
@@ -42,8 +45,16 @@ class _DataBaseCortex(_DataBaseBase):
                 user_id=user_id                                                    \
                 )
         if user is None:
-            logger.error('user {user_id} not found!')
+            logger.warning('user {user_id} not found!')
         return user
+    def get_all_users(self, *, user_id):
+        users =                                                                     \
+            self.driver.get_entities(                                               \
+                _DataBaseCortex.ENTITY_USER                                         \
+                )
+        if users is None:
+            logger.warning('no users found!')
+        return users
     def update_user(self, *, user_id, **kwargs):
         update_result =                                                             \
             self.driver.update_entity(                                              \
@@ -53,16 +64,14 @@ class _DataBaseCortex(_DataBaseBase):
                 **kwargs                                                            \
                 ) 
         if not update_result:
-            logger.error('error while updating user {user_id} to {kwargs_to_string(**kwargs)}')
+            logger.warning('error while updating user {user_id} to {kwargs_to_string(**kwargs)}')
     # Snapshot Entity Section
-    def create_snapshot(self, *, snapshot_uuid, snapshotname, birth_date, gender):
+    def create_snapshot(self, *, snapshot_uuid, timestamp):
         return                                                                      \
             self.driver.create_entity(                                              \
                 _DataBaseCortex.ENTITY_SNAPSHOT,                                    \
                 snapshot_uuid=snapshot_uuid,                                        \
-                snapshotname=snapshotname,                                          \
-                birth_date=birth_date,                                              \
-                gender=gender                                                       \
+                timestamp=timestamp,                                                \
                 )
     def get_snapshot(self, *, snapshot_uuid):
         snapshot =                                                                  \
@@ -71,6 +80,128 @@ class _DataBaseCortex(_DataBaseBase):
                 snapshot_uuid=snapshot_uuid                                         \
                 )
         if snapshot is None:
-            logger.error('snapshot {snapshot_uuid} not found!')
+            logger.warning('snapshot {snapshot_uuid} not found!')
         return snapshot
+    def get_user_snapshots(self, *, user_id):
+        snapshots =                                                                 \
+            self.driver.get_entities(                                               \
+                _DataBaseCortex.ENTITY_SNAPSHOT,                                    \
+                user_id=user_id                                                     \
+                )
+        if snapshots is None:
+            logger.warning('user {user_id} snapshots not found!')
+        return snapshots
+    def get_snapshot_details(self, *, snapshot_uuid):
+        snapshot =                                                                  \
+            self.driver.get_entity(                                                 \
+                _DataBaseCortex.ENTITY_SNAPSHOT,                                    \
+                snapshot_uuid=snapshot_uuid                                         \
+                )
+        if snapshot is None:
+            logger.warning('snapshot {snapshot_uuid} not found!')
+        # Adding available results to snapshot details
+        snapshot.update( { _DataBaseCortex.AVAILABLE_RESULTS_LIST_NAME : [] } )
+        for parsed_entity_name in _DataBaseCortex.PARSED_ENTITIES_LIST:
+            if self.driver.has_entity(parsed_entity_name):
+                snapshot[_DataBaseCortex.AVAILABLE_RESULTS_LIST_NAME].append(parsed_entity_name)
+        return snapshot
+    # Pose Entity Section
+    def create_pose(self, *,            \
+                    snapshot_uuid,      \
+                    translation_x,      \
+                    translation_y,      \
+                    translation_z,      \
+                    rotation_x,         \
+                    rotation_y,         \
+                    rotation_z,         \
+                    rotation_w,         \
+                    ):
+        return                                                                      \
+            self.driver.create_entity(                                              \
+                _DataBaseCortex.ENTITY_POSE,                                        \
+                snapshot_uuid   = snapshot_uuid,                                    \
+                translation_x   = translation_x,                                    \
+                translation_y   = translation_y,                                    \
+                translation_z   = translation_z,                                    \
+                rotation_x      = rotation_x,                                       \
+                rotation_y      = rotation_y,                                       \
+                rotation_z      = rotation_z,                                       \
+                rotation_w      = rotation_w,                                       \
+                )
+    def get_pose(self, *, snapshot_uuid):
+        pose =                                                                      \
+            self.driver.get_entity(                                                 \
+                _DataBaseCortex.ENTITY_POSE,                                        \
+                snapshot_uuid=snapshot_uuid                                         \
+                )
+        if pose is None:
+            logger.warning('pose for snapshot {snapshot_uuid} not found!')
+        return pose
+    # User Feelings Entity Section
+    def create_user_feelings(self, *,   \
+                    snapshot_uuid,      \
+                    hunger,             \
+                    thirst,             \
+                    exhaustion,         \
+                    happiness,          \
+                    ):
+        return                                                                      \
+            self.driver.create_entity(                                              \
+                _DataBaseCortex.ENTITY_USER_FEELINGS,                               \
+                snapshot_uuid   = snapshot_uuid,                                    \
+                hunger          = hunger,                                           \
+                thirst          = thirst,                                           \
+                exhaustion      = exhaustion,                                       \
+                happiness       = happiness,                                        \
+                )
+    def get_user_feelings(self, *, snapshot_uuid):
+        user_feelings =                                                             \
+            self.driver.get_entity(                                                 \
+                _DataBaseCortex.ENTITY_USER_FEELINGS,                               \
+                snapshot_uuid=snapshot_uuid                                         \
+                )
+        if user_feelings is None:
+            logger.warning('user feelings for snapshot {snapshot_uuid} not found!')
+        return user_feelings
+    # User Color Image Entity Section
+    def create_color_image(self, *,     \
+                    snapshot_uuid,      \
+                    uri,                \
+                    ):
+        return                                                                      \
+            self.driver.create_entity(                                              \
+                _DataBaseCortex.ENTITY_COLOR_IMAGE,                                 \
+                snapshot_uuid   = snapshot_uuid,                                    \
+                uri             = uri                                               \
+                )
+    def get_color_image(self, *, snapshot_uuid):
+        color_image =                                                               \
+            self.driver.get_entity(                                                 \
+                _DataBaseCortex.ENTITY_COLOR_IMAGE,                                 \
+                snapshot_uuid=snapshot_uuid                                         \
+                )
+        if color_image is None:
+            logger.warning('color image for snapshot {snapshot_uuid} not found!')
+        return color_image
+    # User Depth Image Entity Section
+    def create_depth_image(self, *,     \
+                    snapshot_uuid,      \
+                    uri,                \
+                    ):
+        return                                                                      \
+            self.driver.create_entity(                                              \
+                _DataBaseCortex.ENTITY_DEPTH_IMAGE,                                 \
+                snapshot_uuid   = snapshot_uuid,                                    \
+                uri             = uri                                               \
+                )
+    def get_depth_image(self, *, snapshot_uuid):
+        depth_image =                                                               \
+            self.driver.get_entity(                                                 \
+                _DataBaseCortex.ENTITY_DEPTH_IMAGE,                                 \
+                snapshot_uuid=snapshot_uuid                                         \
+                )
+        if depth_image is None:
+            logger.warning('depth image for snapshot {snapshot_uuid} not found!')
+        return depth_image
+    
     
