d392d8812c1999f8b0b49301da155b4a96e8ad1f
AvivYaniv <avivyaniv@gmx.com>
2020-05-02 13:07:48 +0300

    Better general embedding of data  in GUI

--- cortex/gui/gui_server.py
+++ cortex/gui/gui_server.py
@@ -15,7 +15,7 @@ API_URL_USERS_FORMAT        = 'http://localhost:5000/api/v1.0/users'
 API_URL_SNAPSHOTS_FORMAT    = 'http://localhost:5000/api/v1.0/users/{}/results'
 
 # Flask
-app = Flask(__name__, static_url_path='', static_folder=GUI_CLIENT_FOLDER, template_folder=GUI_CLIENT_FOLDER)
+app     = Flask(__name__, static_url_path='', static_folder=GUI_CLIENT_FOLDER, template_folder=GUI_CLIENT_FOLDER)
 
 def read_client_file(fname, folder=GUI_CLIENT_FOLDER):
     file_path = Path(folder, fname)
@@ -28,6 +28,12 @@ def read_client_file(fname, folder=GUI_CLIENT_FOLDER):
     return file_content
 
 def gui_serever():
+    def embed_data_in_page(page, **kwargs):
+        embedded_page = page
+        for key, value in kwargs.items():
+            embedded_page = embedded_page.replace(f'%{key}%', value)
+        return embedded_page
+                
     def embed_data_in_index(raw_index):
         users_json                  =   requests.get(API_URL_USERS_FORMAT).json()
         users_converted             =   []
@@ -36,23 +42,30 @@ def gui_serever():
             user_id                 =   user_json['user_id']
             username                =   user_json['username']
             users_converted.append([f'{username}', f'{user_id}'])        
-        index_embedded              =   raw_index.replace('%users_converted%', str(users_converted))
+        index_embedded              =   embed_data_in_page(raw_index, users_converted=str(users_converted))
         return index_embedded
     
     def embed_data_in_snapshots(raw_snapshots, user_id):
         user_snapshots_url          =   API_URL_SNAPSHOTS_FORMAT.format(user_id)
         snapshots_json_as_string    =   requests.get(user_snapshots_url).text    
-        snapshots_embedded          =   raw_snapshots.replace('%snapshots_data%', snapshots_json_as_string)
+        snapshots_embedded          =   embed_data_in_page(raw_snapshots, snapshots_data=snapshots_json_as_string)        
         user_info_url               =   API_URL_USER_INFO_FORMAT.format(user_id)
         user_info_json              =   json.loads(requests.get(user_info_url).json())
-        snapshots_embedded          =   snapshots_embedded.replace('%username%', user_info_json['username'])
-        snapshots_embedded          =   snapshots_embedded.replace('%user_id%', str(user_info_json['user_id']))
-        snapshots_embedded          =   snapshots_embedded.replace('%birth_date%', user_info_json['birth_date'])
-        snapshots_embedded          =   snapshots_embedded.replace('%gender%', user_info_json['gender'])
+        snapshots_embedded          =   embed_data_in_page(snapshots_embedded,                              \
+                                                           username     =   user_info_json['username'],     \
+                                                           user_id      =   str(user_info_json['user_id']), \
+                                                           birth_date   =   user_info_json['birth_date'],   \
+                                                           gender       =   user_info_json['gender'])
         return snapshots_embedded
     
+    # General error handler to disclose actual error code
+    @app.errorhandler(Exception)
+    def server_error_page(e):
+        # TODO Log
+        return app.send_static_file('error.html'), 200
+    
     @app.route('/')
-    def index(): 
+    def index():        
         raw_index           = read_client_file('index.html')
         embedded_index      = embed_data_in_index(raw_index)          
         return embedded_index, 200        
