9e6658c6f9faef9c02030757df663b7dd2c93a91
AvivYaniv <avivyaniv@gmx.com>
2020-05-13 23:34:33 +0300

    Try bind to '0.0.0.0'

--- tests/client/test_client_service.py
+++ tests/client/test_client_service.py
@@ -10,7 +10,7 @@ import pytest
 from tests.integration.tools.main_create_example_mind import EXAMPLE_FILE_PATH, DEFAULT_FILE_VERSION
 
 from cortex.client.client_service import ClientService
-from cortex.client.client_service import DEFAULT_PORT
+# from cortex.client.client_service import 
 
 from cortex.utils import delete_under_folder
 
@@ -20,6 +20,7 @@ from cortex.server.server_handler import ServerHandler
 from tests.test_constants import *
 
 DEFAULT_HOST = '0.0.0.0'
+DEFAULT_PORT = 1234
     
 def delete_server_user_folder_before_and_after(function):
     @functools.wraps(function)
