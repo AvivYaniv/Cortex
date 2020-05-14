68433c093cfaf66c4593d4ee5e3ae801d9a86a47
AvivYaniv <avivyaniv@gmx.com>
2020-05-14 10:03:04 +0300

    CI/CD
    E 32

--- tests/client/test_client_service.py
+++ tests/client/test_client_service.py
@@ -47,5 +47,5 @@ def test_client_service(client_service, capsys):
     server_thread.start()
     client_service.upload_sample(EXAMPLE_FILE_PATH, DEFAULT_FILE_VERSION)
     time.sleep(SERVER_SNAPSHOT_MAX_DURATION_HANDLING * client_service.total_snapshots_uploaded)    
-    server_thread.kill()    
+    #server_thread.kill()    
     assert client_service.total_snapshots_uploaded == test_server_snapshot_published_counter.value
