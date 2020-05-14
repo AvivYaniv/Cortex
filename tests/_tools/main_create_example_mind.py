929a237620222d052d91e274a907954f35316c1d
AvivYaniv <avivyaniv@gmx.com>
2020-05-14 15:33:22 +0300

    Update test file creation

--- tests/_tools/main_create_example_mind.py
+++ tests/_tools/main_create_example_mind.py
@@ -2,19 +2,22 @@
 from cortex.readers import MindFileReader
 from cortex.readers.reader_versions import ReaderVersions
 
+from tests.test_constants import get_user_test_file_path
+from tests.test_constants import TEST_USER_1_ID, TEST_USER_2_ID
+
 # Change working directory to main directory
 import os
 from cortex.writers.mind.mind_writer import MindFileWriter
-os.chdir('../../../')
+os.chdir('../../')
 
 # Constants Section
 EXAMPLE_SNAPSHOTS_NUMBER    =   5
 DEFAULT_FILE_PATH           =   'sample.mind.gz'
-EXAMPLE_FILE_PATH           =   'example.mind.gz'
+
 DEFAULT_FILE_VERSION        =   ReaderVersions.PROTOBUFF 
 DEPRECTED_FILE_VERSION      =   [ ReaderVersions.BINARY ]   
 
-def create_example_mind(file_path='', version=''):
+def create_example_mind(file_path='', version='', example_user_id=''):
     file_path           = file_path if file_path else DEFAULT_FILE_PATH
     version             = version if version else DEFAULT_FILE_VERSION
     
@@ -22,9 +25,13 @@ def create_example_mind(file_path='', version=''):
         print(f'ERROR! {version} is deprecated - won\'t export to example file')
         return
     
+    example_file_path = get_user_test_file_path(example_user_id)
+    
     with MindFileReader(file_path, version) as sample_reader:
-        with MindFileWriter(EXAMPLE_FILE_PATH, version) as sample_writer:
+        with MindFileWriter(example_file_path, version) as sample_writer:
             user_information     =     sample_reader.user_information
+            if example_user_id:
+                user_information.user_id = int(example_user_id)
             sample_writer.write_user_information(user_information)            
             snapshots_counter = 0
             for snapshot in sample_reader:                
@@ -33,14 +40,15 @@ def create_example_mind(file_path='', version=''):
                 if EXAMPLE_SNAPSHOTS_NUMBER == snapshots_counter:                    
                     break
         
-    with MindFileReader(EXAMPLE_FILE_PATH, version) as sample_reader:
+    with MindFileReader(example_file_path, version) as sample_reader:
         user_information     =     sample_reader.user_information            
         snapshots_counter = 0
         for snapshot in sample_reader:                
             snapshots_counter += 1
-            print(snapshots_counter)
+            
+    assert EXAMPLE_SNAPSHOTS_NUMBER == snapshots_counter 
         
-    print(f'Exported {file_path} ---> {EXAMPLE_FILE_PATH} successfully!')
+    print(f'Exported {file_path} ---> {example_file_path} successfully!')
         
 if "__main__" == __name__:
-    create_example_mind()   
+    create_example_mind(example_user_id=TEST_USER_1_ID)   
--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,9 +1,16 @@
 
 # User Section
-TEST_USER_ID                            =   '42'
+TEST_USER_1_ID                          =   '42'
+TEST_USER_2_ID                          =   '100'
 
 # Durations Section
 SERVER_SNAPSHOT_MAX_DURATION_HANDLING   =   1
 
 # Hosts
-SERVER_TEST_HOST                        = '0.0.0.0'
+SERVER_TEST_HOST                        =   '0.0.0.0'
+
+# File names
+EXAMPLE_FILE_PATH_FORMAT                =   'example%s.mind.gz'
+
+def get_user_test_file_path(user_id):
+    return EXAMPLE_FILE_PATH_FORMAT % user_id
