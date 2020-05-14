6c862d172be81cb2c362fab8661a304f7df73580
AvivYaniv <avivyaniv@gmx.com>
2020-05-14 17:38:19 +0300

    Test files to absolute path

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,4 +1,8 @@
 
+import pathlib
+from inspect import getsourcefile
+from os.path import abspath
+
 # User Section
 TEST_USER_1_ID                          =   '42'
 TEST_USER_2_ID                          =   '100'
@@ -12,5 +16,8 @@ SERVER_TEST_HOST                        =   '0.0.0.0'
 # File names
 EXAMPLE_FILE_PATH_FORMAT                =   'example%s.mind.gz'
 
+def project_root():
+    return str(pathlib.Path(abspath(getsourcefile(lambda:0))).parent.parent)
+
 def get_user_test_file_path(user_id):
-    return EXAMPLE_FILE_PATH_FORMAT % user_id
+    return str(pathlib.Path(project_root(), EXAMPLE_FILE_PATH_FORMAT % user_id))
