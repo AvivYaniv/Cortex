69de890b1f50393a9b51fce15dc2a6bdcbbf9e3a
AvivYaniv <avivyaniv@gmx.com>
2020-05-19 02:28:02 +0300

    Update rabbitmq_shutdown.sh

--- cortex/publisher_consumer/message_queue/rabbitmq_shutdown.sh
+++ cortex/publisher_consumer/message_queue/rabbitmq_shutdown.sh
@@ -2,5 +2,7 @@
 
 ( 
 	echo "Shutdown rabbitmq-server"
-	sudo systemctl disable rabbitmq-server	
+	sudo rabbitmqctl stop
+	sudo update-rc.d -f rabbitmq-server remove
+ 	sudo systemctl disable rabbitmq-server	
 )
