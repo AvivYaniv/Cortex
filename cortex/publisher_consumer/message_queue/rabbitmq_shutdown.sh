fd7752858f26ad23eb41b069c470b88fc4884edf
AvivYaniv <avivyaniv@gmx.com>
2020-05-19 20:42:53 +0300

    Update rabbitmq_shutdown.sh

--- cortex/publisher_consumer/message_queue/rabbitmq_shutdown.sh
+++ cortex/publisher_consumer/message_queue/rabbitmq_shutdown.sh
@@ -2,9 +2,6 @@
 
 ( 
 	echo "Shutdown rabbitmq-server"
-	# If not on Travis (where not priviledged to stop service)
-	if [ -z "$TRAVIS" ]; then
-		sudo rabbitmqctl stop
-		sudo invoke-rc.d rabbitmq-server stop
-	fi	
+	sudo rabbitmqctl stop
+	sudo invoke-rc.d rabbitmq-server stop
 )
