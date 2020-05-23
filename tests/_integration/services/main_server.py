dd28f5b5f2544ec5d127424ac3df4de4bbe89023
AvivYaniv <avivyaniv@gmx.com>
2020-05-23 20:10:38 +0300

    Move server patching from integration to parser sniffer

--- tests/_integration/services/main_server.py
+++ tests/_integration/services/main_server.py
@@ -1,34 +1,13 @@
 
 from cortex.server import run_server
 
-import cortex.server.server_handler
-
-from cortex.server.server_handler import ServerHandler
-
-import cortex.utils.consts
-
-from tests.test_constants import get_raw_snapshot_folder_path, get_raw_snapshot_folder_relative_path
-
 from cortex.utils import change_direcoty_to_project_root
-
-from cortex.utils import delete_under_folder
 
 @change_direcoty_to_project_root()
 def run_server_at_root_directory(host, port):
-    delete_under_folder(get_raw_snapshot_folder_path())
     run_server(host, port)
-
-def _get_snapshot_save_path(self, snapshot_uuid):
-    return cortex.server.server_handler.ServerHandler._get_save_path(  \
-                get_raw_snapshot_folder_relative_path(),               \
-                cortex.server.server_handler.SNAPSHOT_FILE_NAME) 
-    
-def patch_server_save_file_path():    
-    cortex.server.server_handler.ServerHandler._get_snapshot_save_path = \
-        _get_snapshot_save_path
     
 if "__main__" == __name__:
-    host, port = '127.0.0.1', '8000'
-    patch_server_save_file_path()
+    host, port = '127.0.0.1', '8000'    
     run_server_at_root_directory(host, port)
     
--- tests/_tools/main_parserer_sniffer.py
+++ tests/_tools/main_parserer_sniffer.py
@@ -1,4 +1,16 @@
 
+from cortex.server import run_server
+
+import cortex.server.server_handler
+
+from cortex.server.server_handler import ServerHandler
+
+import cortex.utils.consts
+
+from tests.test_constants import get_raw_snapshot_folder_path, get_raw_snapshot_folder_relative_path
+
+from cortex.utils import delete_under_folder
+
 from tests._utils.message_writer import write_messages_class_method_decorator
 
 import multiprocessing
@@ -32,7 +44,28 @@ def run_parser_proceess(parser_type):
     parser_proccess = multiprocessing.Process(target=run_patched_parser_at_root_directory, args=[parser_type])
     parser_proccess.start()
 
+def run_patched_server():
+    @change_direcoty_to_project_root()
+    def run_server_at_root_directory(host, port):
+        delete_under_folder(get_raw_snapshot_folder_path())
+        run_server(host, port)
+    def _get_snapshot_save_path(self, snapshot_uuid):
+        return cortex.server.server_handler.ServerHandler._get_save_path(  \
+                    get_raw_snapshot_folder_relative_path(),               \
+                    cortex.server.server_handler.SNAPSHOT_FILE_NAME)         
+    def patch_server_save_file_path():    
+        cortex.server.server_handler.ServerHandler._get_snapshot_save_path = \
+            _get_snapshot_save_path  
+    host, port = '127.0.0.1', '8000'
+    patch_server_save_file_path()
+    run_server_at_root_directory(host, port)
+
+def run_server_process():
+    server_proccess = multiprocessing.Process(target=run_patched_server)
+    server_proccess.start()
+
 if "__main__" == __name__:
+    run_server_process()
     for parser_type in PARSER_TYPES:
         run_parser_proceess(parser_type)
         
