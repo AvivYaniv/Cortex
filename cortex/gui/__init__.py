13d0f8286854be3d1c938a6815489d9d13080069
AvivYaniv <avivyaniv@gmx.com>
2020-05-29 23:31:51 +0300

    API refactor

--- cortex/gui/__init__.py
+++ cortex/gui/__init__.py
@@ -1,2 +1,4 @@
 
+from cortex.gui.gui_server import run_server  
+
 version = '0.1.0'
--- cortex/gui/gui_server.py
+++ cortex/gui/gui_server.py
@@ -104,6 +104,9 @@ def gui_serever():
         embedded_snapshots  = embed_data_in_snapshots(raw_snapshots, user_id)
         return embedded_snapshots, 200        
 
+def run_server(host='', port='', api_host='', api_port=''):
+    run_gui_server(host, port, api_host, api_port)
+    
 def run_gui_server(host='', port='', api_host='', api_port=''):
     """Starts an GUI server of which users and snapshots can be served to client side in convenient way"""
     gui_serever()
